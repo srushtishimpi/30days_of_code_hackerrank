d9f34729da21fa015e747bc2fab6c39c2c3ca9fa
srushtishimpi <55121414+srushtishimpi@users.noreply.github.com>
2019-09-16 13:18:46 -0400

    Update and rename day1.py to day0.py

---
+++ day_0/day0.py
@@ -0,0 +1,8 @@
+# Take input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+ 
+# Print string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+ 
+# Prints the contents of input_string to stdout.
+print(input_string)
