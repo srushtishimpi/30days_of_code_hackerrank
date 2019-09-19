963fb6b5518f2da6bd1904482b5d74913ae11095
srushtishimpi <55121414+srushtishimpi@users.noreply.github.com>
2019-09-18 21:54:30 -0400

    Create day3.py

---
+++ day_3/day3.py
@@ -0,0 +1,22 @@
+#Code by: Srushti Shimpi
+#Hackerrank Day 3 Challange
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+
+if((n%2)!= 0 ):
+    print('Weird')
+
+elif((n%2)==0 and ((2<=n<=5) or (n>20))):
+    print('Not Weird')
+
+elif((n%2)==0 and (6<=n<=20)):
+    print('Weird')
