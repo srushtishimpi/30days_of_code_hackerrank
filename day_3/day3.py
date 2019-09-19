de4b24cedc4f47d4c88cbd5337a321464b198cd7
srushtishimpi <55121414+srushtishimpi@users.noreply.github.com>
2019-09-18 22:40:03 -0400

    Update day3.py

--- day_3/day3.py
+++ day_3/day3.py
@@ -12,11 +12,14 @@ import sys
 if __name__ == '__main__':
     n = int(input())
 
-if((n%2)!= 0 ):
-    print('Weird')
+    if((n%2)!= 0 ):
+        print('Weird')
 
-elif((n%2)==0 and ((2<=n<=5) or (n>20))):
-    print('Not Weird')
+    elif((n%2)==0 and ((2<=n<=5) or (n>20))):
+        print('Not Weird')
 
-elif((n%2)==0 and (6<=n<=20)):
-    print('Weird')
+    elif((n%2)==0 and (6<=n<=20)):
+        print('Weird')
+
+
+    
