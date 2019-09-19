bef7851318f6d03bfb44c74b094ea98ca19a2dd0
srushtishimpi <55121414+srushtishimpi@users.noreply.github.com>
2019-09-19 10:28:44 -0400

    Create day4.py

---
+++ day_4/day4.py
@@ -0,0 +1,26 @@
+#Code by: Srushti Shimpi
+#Hackerrank Day 4 Challange
+
+class Person:
+    def __init__(self, initialAge):
+        # Add some more code to run some checks on initialAge
+        self.age = initialAge
+        if initialAge < 0:
+            print("Age is not valid, setting age to 0.")
+        else:
+            self.age = initialAge
+    
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        if(self.age < 13):
+            print("You are young.")
+        elif(13 <= self.age < 18):
+            print("You are a teenager.")
+        elif(self.age >= 18):
+            print("You are old.")
+
+    def yearPasses(self):    
+        # Increment the age of the person in here
+        self.age += 1
+
+t = int(input())
