58cf5e657744f74639975fdbe20cd5fa6a0752f3
srushtishimpi <55121414+srushtishimpi@users.noreply.github.com>
2019-09-20 18:54:55 -0400

    Create day5.py

---
+++ day_5/day5.py
@@ -0,0 +1,23 @@
+  
+#Code by: Srushti Shimpi
+#Hackerrank Day 5 Challange
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(10):
+        i = i+1
+        result = n*i
+        print(n, "x",  i, "=", result)
+
+       
